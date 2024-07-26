d900ac96e19e22f32f3c28fc39f2cbb1fea0aebb
luannp132 <luanl0905123598@gmail.com>
2024-07-26 14:10:54 +0700

    ...

--- resources/views/frontend/layouts/app-user.blade.php
+++ resources/views/frontend/layouts/app-user.blade.php
@@ -1,4 +1,5 @@
-@include('layouts/header')
+@include('layouts/head')
+@include('layouts/nav')
    
     
 @yield('main')
---
+++ resources/views/frontend/layouts/head.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <base href="http://127.0.0.1:8000/">
+    <!-- ------------- META TAG START HERE ------------- -->
+    <meta charset="UTF-8" />
+    <meta name="title" content="Gymfit.com">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no" />
+    <meta name=theme-color content="#1face1" />
+    <meta name="description"
+        content="We craft website in a way which improves the web experience. We take pride in treating all customers with the highest degree of care, understanding, services, and support.">
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="site_url">
+    <meta property="og:title" content="Gymfit.com">
+    <meta property="og:description"
+        content="We craft website in a way which improves the web experience. We take pride in treating all customers with the highest degree of care, understanding, services, and support.">
+    <meta property="og:image" content="https://site_url.com/assets/images/thumbnail.webp">
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="https://site_url.com">
+    <meta property="twitter:title" content="Gymfit.com">
+    <meta property="twitter:description"
+        content="We craft website in a way which improves the web experience. We take pride in treating all customers with the highest degree of care, understanding, services, and support.">
+    <meta property="twitter:image" content="https://site_url.com/assets/images/thumbnail.webp">
+    <!-- ------------- META TAG END'S HERE ------------- -->
+    <title>Gymfit.com</title>
+    <!-- FAVICON -->
+    <link rel="icon" type="image/png" href="assets/images/favicon.svg">
+
+    <!-- LOCAL LINK ATTACHMENT -->
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/bootstrap.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/swiper.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/app.css'>
+
+	<script>
+		const hasSupport = 'loading' in HTMLImageElement.prototype;
+		document.documentElement.className = hasSupport ? 'pass' : 'fail';
+	</script>
+</head>
+
+<body>
+    <!-- LOADER START HERE -->
+    <div class="page_loader">
+        <img loading='lazy' src="assets/images/loader.svg" alt="img">
+    </div>
+    <!-- LOADER END HERE -->
+    <!-- HEADER START HERE -->
+    
+    
+
+   
