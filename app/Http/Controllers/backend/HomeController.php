9ac3e8d33eb40930dc17f085441b35b58fe64806
BiUsagi <tubanovel@gmail.com>
2024-07-26 15:01:15 +0700

    123

--- app/Http/Controllers/backend/HomeController.php
+++ app/Http/Controllers/backend/HomeController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers\frontend;
+namespace App\Http\Controllers\backend;
 
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
