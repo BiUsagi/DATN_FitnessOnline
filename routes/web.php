49664d6f9012f9375794f84c7892cf4fab5da62b
BiUsagi <tubanovel@gmail.com>
2024-07-26 15:08:23 +0700

    131312

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\HomeController;
+use App\Http\Controllers\backend\AdminController;
 
+
+
+//Front End
 Route::get('/', [HomeController::class, 'index'])->name('index');
 Route::get('/about', [HomeController::class, 'about'])->name('about.index');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact.index');
+
+
+
+// Back End
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
