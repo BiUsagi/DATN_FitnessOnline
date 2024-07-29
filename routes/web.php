c1a33c5d59859b019b571249b4d1e5b79fc2016a
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-07-29 14:32:11 +0700

    123

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/about', [HomeController::class, 'about'])->name('about.index');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact.index');
 
 
-
 // Back End
 Route::get('/admin', [AdminController::class, 'index'])->name('admin');
 
