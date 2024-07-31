f0b75b787b4090616c102e47b254ce50b5e6871b
luannp132 <luanl0905123598@gmail.com>
2024-07-31 12:30:01 +0700

    ...

--- resources/views/backend/layouts/head.blade.php
+++ resources/views/backend/layouts/head.blade.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+  <base href="http://127.0.0.1:8000/">
+
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
@@ -15,7 +17,9 @@
 
   <!-- Google Fonts -->
   <link href="https://fonts.gstatic.com" rel="preconnect">
-  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
 
   <!-- Vendor CSS Files -->
   <link href="assets/backend/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\frontend\HomeController;
 use App\Http\Controllers\backend\AdminController;
+use App\Http\Controllers\backend\OrderController;
 
 
 
@@ -14,6 +15,7 @@ Route::get('/contact', [HomeController::class, 'contact'])->name('contact.index'
 
 // Back End
 Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+Route::get('/admin/order', [OrderController::class, 'index'])->name('admin.order');
 
 
 
