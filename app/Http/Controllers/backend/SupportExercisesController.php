21cd853ddd099e39c0b73d941f2bcb1692b4d049
luannp132 <luanl0905123598@gmail.com>
2024-08-05 15:07:04 +0700

    ..

--- app/Http/Controllers/backend/SupportExercisesController.php
+++ app/Http/Controllers/backend/SupportExercisesController.php
@@ -10,8 +10,8 @@ class SupportExercisesController extends Controller
 {
     public function index()
     {
-        $supportExercises = Support_Exercise::with(['exercise', 'user'])
-            ->select('id', 'content', 'id_exercise', 'id_user', 'created_at')
+        $supportExercises = Support_Exercise::with(['exercise', 'user', 'staff'])
+            ->select('id', 'content', 'id_exercise', 'id_user', 'id_staff', 'created_at')
             ->get()
             ->map(function ($item) {
                 return [
@@ -19,6 +19,7 @@ class SupportExercisesController extends Controller
                     'content' => $item->content,
                     'name_exercise' => $item->exercise->name_exercise ?? 'N/A',
                     'name_user' => $item->user->name ?? 'N/A',
+                    'name_staff' => $item->staff->name_staff ?? 'N/A',
                     'created_at' => $item->created_at,
                 ];
             });
