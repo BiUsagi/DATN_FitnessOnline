94e34d9d9c60e1c4fdae37fe16c5531159678e8b
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-10-08 19:51:25 +0700

    off load

--- resources/views/frontend/layouts/head.blade.php
+++ resources/views/frontend/layouts/head.blade.php
@@ -48,9 +48,9 @@
 
 <body>
     <!-- LOADER START HERE -->
-    <div class="page_loader">
+    {{-- <div class="page_loader">
         <img loading='lazy' src="assets/frontend/images/loader.svg" alt="img">
-    </div>
+    </div> --}}
     <!-- LOADER END HERE -->
     <!-- HEADER START HERE -->
     
