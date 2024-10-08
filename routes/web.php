d7332a63aa0f29b0d1769a3f79f5ec3c73457a4f
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-10-08 19:53:54 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -59,14 +59,11 @@ Route::prefix('admin')->group(function () {
     Route::get('/exerciseset', [ExerciseSetController::class, 'index'])->name('admin.exerciseset');
     //create_goitap
     Route::get('/exerciseset/create', [ExerciseSetController::class, 'create'])->name('admin.exerciseset-create');
-<<<<<<< HEAD
     Route::post('/exerciseset/create',[ExerciseSetController::class, 'create_'])->name('admin.exerciseset-create_');
     //update_goi_tap
     Route::get('/exerciseset/update', [ExerciseSetController::class, 'update'])->name('admin.exerciseset-update');
 
-=======
     Route::post('/exerciseset/create', [ExerciseSetController::class, 'create_'])->name('admin.exerciseset-create_');
->>>>>>> 134792c8376ddcc39c5dfdb25a92a4293d0a8d12
 
     // statistical - thống kê
     Route::get('/statistical', [StatisticalController::class, 'index'])->name('admin.statistical');
