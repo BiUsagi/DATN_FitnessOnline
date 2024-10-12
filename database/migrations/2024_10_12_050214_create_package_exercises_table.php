f9d4358d805751e9f477becfa83c0b63cf970dcf
BiUsagi <tubanovel@gmail.com>
2024-10-12 12:07:25 +0700

    database by sơn

---
+++ database/migrations/2024_10_12_050214_create_package_exercises_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('package_exercises', function (Blueprint $table) {
+            $table->id(); // ID chính (PK)
+            $table->unsignedBigInteger('exercise_id'); // ID bài tập (FK)
+            $table->unsignedBigInteger('workout_packages_id'); // ID gói tập (FK)
+            $table->timestamps();
+
+            // Khóa ngoại (foreign key) liên kết đến bảng Exercise
+            $table->foreign('exercise_id')->references('id')->on('exercise')->onDelete('cascade');
+
+            // Khóa ngoại (foreign key) liên kết đến bảng WorkoutPackages
+            $table->foreign('workout_packages_id')->references('id')->on('workout_packages')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('package_exercises');
+    }
+};
