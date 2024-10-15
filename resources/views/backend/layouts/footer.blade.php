7a3258fda926bd1ddedebab7219ac3e84ae69937
BiUsagi <tubanovel@gmail.com>
2024-10-15 19:22:04 +0700

    123

--- resources/views/backend/layouts/footer.blade.php
+++ resources/views/backend/layouts/footer.blade.php
@@ -39,6 +39,9 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
 
+{{-- Nhúng js từ các views --}}
+@yield('custom_js')
+
 </body>
 
 </html>
