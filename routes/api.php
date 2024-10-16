91d48f0898765a4f3dabbee604d7c833b0fd4933
BiUsagi <tubanovel@gmail.com>
2024-10-16 16:40:00 +0700

    fix by sơn

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,19 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\backend\api\PackageExercisesController;
 use App\Http\Controllers\backend\api\ExerciseController;
+use App\Http\Controllers\backend\api\AccountsController;
 
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'admin',], function (){
+Route::group(['prefix' => 'admin',], function () {
     Route::get('/baitap', [ExerciseController::class, 'index']);
+
+
+
+    // account
+    Route::get('/get-user/{id}', [AccountsController::class, 'getUser'])->name('api.user');
+    Route::post('/update-user', [AccountsController::class, 'updateUser'])->name('api.user.update');
 });
