7d88e2ca65450e29e1a1dde72bc241f07c7a8f30
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-10-19 14:47:00 +0700

    update migate workout_exercise

--- database/migrations/2024_10_12_050127_create_workout_packages_table.php
+++ database/migrations/2024_10_12_050127_create_workout_packages_table.php
@@ -11,17 +11,17 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::create('workout_packages', function (Blueprint $table) {
-            $table->id();                       // Khóa chính
-            $table->foreignId('pt_id')          // Khóa ngoại liên kết đến huấn luyện viên
-                  ->constrained('staff')
-                  ->onDelete('cascade');
-            $table->string('name');             // Tên gói tập
-            $table->text('description')->nullable(); // Mô tả gói tập
+            $table->id(); // ID chính (PK)
+            $table->string('package_name'); // Tên gói
+            $table->text('image')->nullable(); //Hình ảnh
+            $table->text('description'); // Mô tả gói
+            $table->string('level'); // Cấp độ (ví dụ Beginner, Intermediate, Advanced)
+            $table->decimal('price', 8, 2); // Giá gói
+            $table->integer('duration_days'); // Thời lượng gói (ngày, tháng...)
+            $table->unsignedBigInteger('staff_id'); // ID huấn luyện viên (FK)
             $table->string('goal')->nullable(); // Mục tiêu tập luyện
-            $table->integer('duration_days');   // Thời gian lộ trình (30, 60, 90 ngày, v.v.)
-            $table->timestamps();                // Thời gian tạo và cập nhật
+            $table->timestamps();                // Thời gian tạo và cập nhật             // Thời gian tạo và cập nhật
         });
-        
     }
 
     /**
