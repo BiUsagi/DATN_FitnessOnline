dab4f99cfabc3020d11e06a6492142a54bf833e2
BiUsagi <tubanovel@gmail.com>
2024-10-26 13:35:16 +0700

    update models

--- app/Models/Workout_Package.php
+++ app/Models/Workout_Package.php
@@ -9,4 +9,27 @@ class Workout_Package extends Model
 {
     use HasFactory;
     protected $table = 'workout_packages';
+
+
+    protected $fillable = [
+        'package_name',
+        'image',
+        'description',
+        'level',
+        'price',
+        'duration_days',
+        'staff_id',
+        'goal',
+        'status',
+    ];
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class, 'workout_package_id');
+    }
+
+    public function staff()
+    {
+        return $this->belongsTo(Staff::class, 'staff_id');
+    }
 }
