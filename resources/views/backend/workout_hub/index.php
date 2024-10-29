693167db9f20059ad6acd6d0a5077c65de6df149
Tuấnu <minhtuan21102004@an21102004gmail.com>
2024-10-29 11:56:27 +0700

    UI workout_hub

---
+++ resources/views/backend/workout_hub/index.php
@@ -0,0 +1,346 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <base href='http://127.0.0.1:8000/'>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/backend/css/workout_hub.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body>
+    <header>
+        <div class="header-block">
+            <div class="block-left">
+                <div class="name-workout">
+                    <p><a href="/admin"><i class="fa-solid fa-chevron-left"></i></a> Bài tập giảm cân cho người mới</p>
+                </div>
+            </div>
+            <div class="block-right">
+                <div class="duration">
+                    <p>Thời lượng</p>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div id="main">
+        <div id="content">
+            <div class="title-day">
+                <h2>Ngày 1</h2>
+                <p class="line"></p>
+                <div class="categories">
+                    <div class="level">
+                        <i class="fa-solid fa-cloud-bolt"></i>
+                        <h3>Mức độ</h3>
+                        <p>Người mới bắt đầu</p>
+                    </div>
+                    <div class="level">
+                        <i class="fa-solid fa-dumbbell"></i>
+                        <h3>Bài tập</h3>
+                        <p>8</p>
+                    </div>
+                    <div class="level">
+                        <i class="fa-solid fa-trophy"></i>
+                        <h3>Bài tập</h3>
+                        <p>Giảm cân</p>
+                    </div>
+                    <div class="level">
+                        <i class="fa-solid fa-stopwatch"></i>
+                        <h3>Thời lượng</h3>
+                        <p>33:15</p>
+                    </div>
+                </div>
+            </div>
+            <div class="list-exercise">
+                <!-- 1 -->
+                <div class="box-exercise">
+                    <div class="action">
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+                    <div class="stt-exercise">
+                        <p><span>1</span> <i class="fa-solid fa-dumbbell"></i></p>
+                    </div>
+                    <div class="img-exercise">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSwEdwzndVi5stp1KtZ3PbEtj79dcFq-AAFw&s" alt="">
+                        <div class="action-start">
+                            <i class="fa-solid fa-play"></i>
+                        </div>
+                    </div>
+                    <div class="infor-exercise">
+                        <h3>Squat nâng tạ</h3>
+                        <p class="level-exercise">Người mới bắt đầu</p>
+                        <div class="rep-set">
+                            <p><i class="fa-solid fa-dumbbell"></i> 8 reps</p>
+                            <p><i class="fa-solid fa-repeat"></i> 3 set</p>
+                        </div>
+                        <div class="tool">
+                            <p>Dụng cụ: Không có</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- 2 -->
+                <div class="box-exercise">
+                    <div class="action">
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+                    <div class="stt-exercise">
+                        <p><span>2</span> <i class="fa-solid fa-dumbbell"></i></p>
+                    </div>
+                    <div class="img-exercise">
+                        <img src="https://thethaodonga.com/wp-content/uploads/2022/01/Sergi-Constance-4.jpg" alt="">
+                        <div class="action-start">
+                            <i class="fa-solid fa-play"></i>
+                        </div>
+                    </div>
+                    <div class="infor-exercise">
+                        <h3>Đẩy tạ nằm</h3>
+                        <p class="level-exercise">Người mới bắt đầu</p>
+                        <div class="rep-set">
+                            <p><i class="fa-solid fa-dumbbell"></i> 8 reps</p>
+                            <p><i class="fa-solid fa-repeat"></i> 3 set</p>
+                        </div>
+                        <div class="tool">
+                            <p>Dụng cụ: Không có</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-exercise">
+                    <div class="action">
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+                    <div class="stt-exercise">
+                        <p><span>3</span> <i class="fa-solid fa-dumbbell"></i></p>
+                    </div>
+                    <div class="img-exercise">
+                        <img src="https://musclefuel.vn/wp-content/uploads/cach-tao-dong-luc-tap-gym.jpg" alt="">
+                        <div class="action-start">
+                            <i class="fa-solid fa-play"></i>
+                        </div>
+                    </div>
+                    <div class="infor-exercise">
+                        <h3>Đẩy tạ qua đầu</h3>
+                        <p class="level-exercise">Người mới bắt đầu</p>
+                        <div class="rep-set">
+                            <p><i class="fa-solid fa-dumbbell"></i> 8 reps</p>
+                            <p><i class="fa-solid fa-repeat"></i> 3 set</p>
+                        </div>
+                        <div class="tool">
+                            <p>Dụng cụ: Không có</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="box-exercise">
+                    <div class="action">
+                        <i class="fa-solid fa-ellipsis-vertical"></i>
+                    </div>
+                    <div class="stt-exercise">
+                        <p><span>4</span> <i class="fa-solid fa-dumbbell"></i></p>
+                    </div>
+                    <div class="img-exercise">
+                        <img src="https://mdfitness.vn/4725841D002D6361/B2FE80C04D5485C94725885A000DAD14/$File/tap-gym-tang-co-bap.jpg" alt="">
+                        <div class="action-start">
+                            <i class="fa-solid fa-play"></i>
+                        </div>
+                    </div>
+                    <div class="infor-exercise">
+                        <h3>Ván tay</h3>
+                        <p class="level-exercise">Người mới bắt đầu</p>
+                        <div class="rep-set">
+                            <p><i class="fa-solid fa-dumbbell"></i> 8 reps</p>
+                            <p><i class="fa-solid fa-repeat"></i> 3 set</p>
+                        </div>
+                        <div class="tool">
+                            <p>Dụng cụ: Không có</p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div id="sidebar">
+            <div class="title">
+                <p>Nội dung khóa tập</p>
+            </div>
+            <div class="list-days">
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 1</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>8 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 2</p>
+                        <span><i class="fa-regular fa-clock"></i> 20:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>9 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 3</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>8 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 4</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>6 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 5</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>8 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 6</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>7 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 7</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>8 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 8</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>5 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 9</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>10 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 10</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>4 bài tập</p>
+                    </div>
+                </div>
+                <div class="box-day">
+                    <div class="info">
+                        <p>Ngày 11</p>
+                        <span><i class="fa-regular fa-clock"></i> 39:22</span>
+                    </div>
+                    <div class="chevron">
+                        <i class="fa-solid fa-book"></i> <p>6 bài tập</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="overflow">
+        <div class="container">
+            <div class="modal">
+                <div class="close-modal-exercise">
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+                <div class="col-left">
+                    <div class="countdown">3</div> 
+                    <div class="overflow-col-left">
+                        <div class="btn-start-exercise">
+                            <a class="start"><i class="fa-solid fa-play"></i></a>
+                        </div>
+                    </div>
+                    <div class="title-detail">
+                        <h3>Hít đất</h3>
+                    </div>
+                    <div class="show-video">
+                        <div class="video-container">
+                            <video >
+                                <source src="/uploads/video_exercise/1730099136.mp4" type="video/mp4">
+                                Trình duyệt của bạn không hỗ trợ video.
+                            </video>
+                            <video >
+                                <source src="/uploads/video_exercise/1730021744.mp4" type="video/mp4">
+                                Trình duyệt của bạn không hỗ trợ video.
+                            </video>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-right">
+                    <div class="title-detail">
+                        <h3>Hướng dẫn tập</h3>
+                    </div>
+                    <div class="content-training">
+                        <div class="content">
+                            <b>Bước 1: Bắt đầu từ tư thế chuẩn bị</b>
+                            <p>Nằm sấp xuống sàn, duỗi thẳng chân về phía sau và để hai bàn chân rộng bằng hông.</p>
+                            <p>Đặt hai tay trên sàn, rộng hơn vai một chút. Đặt tay sao cho ngón cái hướng vào trong và các ngón tay còn lại hướng thẳng lên phía trước.</p>
+                            <p>Dùng mũi chân làm điểm tựa và nâng cơ thể lên, giữ cho lưng, hông và chân tạo thành một đường thẳng.</p>
+    
+                            <b>Bước 2: Thực hiện động tác hạ người xuống</b>
+                            <p>Hít vào và từ từ hạ thấp cơ thể xuống sàn bằng cách gập khuỷu tay.</p>
+                            <p>Giữ khuỷu tay chếch ra một góc khoảng 45 độ so với thân người để tránh gây áp lực quá lớn lên vai.</p>
+                            <p>Hạ thấp người đến khi ngực gần chạm sàn hoặc khuỷu tay tạo thành một góc 90 độ.</p>
+    
+                            <b>Bước 3: Đẩy người lên trở lại</b>
+                            <p>Thở ra, dùng lực từ cơ ngực và cơ tay để đẩy cơ thể lên trở về vị trí ban đầu.</p>
+                            <p>Giữ cơ thể thẳng khi đẩy lên và không để hông bị võng xuống hoặc nâng lên quá cao.</p>
+    
+                            <b class="warning">(*) Một số lưu ý:</b>
+                            <b>Tư thế đúng:</b>
+                            <p>Đảm bảo cơ thể luôn giữ thẳng từ đầu đến gót chân để tránh chấn thương lưng.</p>
+                            <b>Hít thở đúng cách:</b>
+                            <p>Hít vào khi hạ xuống và thở ra khi đẩy lên.</p>
+                            <b>Tốc độ thực hiện:</b>
+                            <p>Không nên thực hiện quá nhanh, tập trung vào việc kiểm soát động tác.</p>
+                        </div>
+                        
+                        <div class="btn-action">
+                            <div class="btn btn-prev">
+                                <p><i class="fa-solid fa-chevron-left"></i> Bài trước</p>
+                            </div>
+                            <div class="btn btn-next">
+                                <p>Bài tiếp theo <i class="fa-solid fa-chevron-right"></i></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <script src="assets/backend/js/workout_hub.js"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -77,6 +77,9 @@ Route::prefix('admin')->group(function () {
     Route::get('/workout_package/create', [WorkoutPackagesController::class, 'create'])->name('admin.workout_package-create');
     //update_goitap
     Route::get('/workout_package/update/{id}', [WorkoutPackagesController::class, 'update'])->name('admin.workout_package-update');
+
+
+    Route::get('/workout_hub', [WorkoutPackagesController::class, 'workout_hub'])->name('admin.workout_hub');
     
     
 
