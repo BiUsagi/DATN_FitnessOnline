77de44970662fd4a53c65b84966e9bc7cfe639f6
rin <nrin2991@gmail.com>
2024-10-29 16:16:11 +0700

    update

--- resources/views/backend/comments/list-report.blade.php
+++ resources/views/backend/comments/list-report.blade.php
@@ -33,6 +33,7 @@
                                     <th>Blog</th>
                                     <th>Ngày đăng</th>
                                     <th class="text-center">Chi tiết</th>
+                                    <th></th>
                                 </tr>
 
                             </thead>
@@ -54,36 +55,39 @@
 <script>
 
    // Hiển thị bình luận bị báo cáo
-$.get('http://127.0.0.1:8000/api/admin/report-comments', function (res) {
-    let data = res;
-    console.log(res);
+   load();
+    function load(){
+        $.get('http://127.0.0.1:8000/api/admin/report-comments', function (res) {
+        let data = res;
+        console.log(res);
 
-    let comments = '';
+        let comments = '';
 
-    data.forEach(sp => {
-        const createdAt = new Date(sp.created_at); // Chuyển đổi chuỗi thành đối tượng Date
-        const formattedDate = createdAt.toLocaleDateString('en-GB'); // Định dạng dd/mm/yyyy
+        data.forEach(sp => {
+            const createdAt = new Date(sp.created_at); // Chuyển đổi chuỗi thành đối tượng Date
+            const formattedDate = createdAt.toLocaleDateString('en-GB'); // Định dạng dd/mm/yyyy
 
-        comments += `
-            <tr>
-                <td class="text-center align-middle">${sp.id}</td>
-                <td class="align-middle">
-                    <img src="assets/backend/img/${sp.avatar}" class="rounded-circle object-fit-cover me-2 avatar-table">
-                    ${sp.user_name}
-                </td>
-                <td class="align-middle text-truncate" style="max-width: 300px;">${sp.content}</td>
-                <td class="align-middle">${sp.title}</td>
-                <td class="align-middle">${formattedDate}</td>
-                <td class="text-center align-middle">
-                    <button type="button" class="btn btn-danger" onclick="deleteComment(${sp.id})">
-                        <i class="ri-delete-bin-5-fill"></i>
-                    </button>
-                </td>
-            </tr>
-        `;
+            comments += `
+                <tr>
+                    <td class="text-center align-middle">${sp.id}</td>
+                    <td class="align-middle">
+                        <img src="assets/backend/img/${sp.avatar}" class="rounded-circle object-fit-cover me-2 avatar-table">
+                        ${sp.user_name}
+                    </td>
+                    <td class="align-middle text-truncate" style="max-width: 300px;">${sp.content}</td>
+                    <td class="align-middle">${sp.title}</td>
+                    <td class="align-middle">${formattedDate}</td>
+                    <td class="text-center align-middle">
+                        <button type="button" class="btn btn-danger" onclick="deleteComment(${sp.id})">
+                            <i class="ri-delete-bin-5-fill"></i>
+                        </button>
+                    </td>
+                </tr>
+            `;
+        });
+        $('#list-items').html(comments);
     });
-    $('#list-items').html(comments);
-});
+    }
 
 function deleteComment(id) {
     // Hiển thị hộp thoại xác nhận bằng SweetAlert2
@@ -108,10 +112,9 @@ function deleteComment(id) {
                         title: 'Thành công!',
                         text: 'Xóa bình luận thành công!',
                         icon: 'success'
-                    }).then(() => {
-                        // Tải lại trang để cập nhật danh sách bình luận
-                        location.reload();
                     });
+                    load();
+                    
                 },
                 error: function (error) {
                     console.log(error);
