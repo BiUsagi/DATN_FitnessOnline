fd5beb026ebf3f03423f17d6ba4300eeaa639329
luannp132 <luanl0905123598@gmail.com>
2024-11-06 18:56:08 +0700

    ..

--- resources/views/frontend/posts/posts-details.blade.php
+++ resources/views/frontend/posts/posts-details.blade.php
@@ -23,7 +23,7 @@
                         
                         <div class="single-blog-post">
                             <div class="blog-image">
-                                <img loading='lazy' src="{{ asset('assets/backend/img/' . $posts->image) }}" alt="blog_detail_img.webp">
+                                <img loading='lazy' src="{{ asset('uploads/post_image/' . $posts->image) }}" alt="blog_detail_img.webp">
                             </div>
                             <div class="blog-detail">
                                 <div class="blog-desc">
